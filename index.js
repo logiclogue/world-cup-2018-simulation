7c98ce85aba1c3d778a46f5ec9d55644355aa493
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-04 20:31:47 +0100

    Updates to simulate the first match of the world cup

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const sim = require("football-score-sim");
+const teamData = require("./src/teams");
 
-console.log(sim);
+const teams = teamData.map(team => new sim.Team(team.name, team.rating));
+
+const russia = teams[0];
+const saudi = teams[1];
+const seed = "testing".toSeed();
+
+const match = new sim.Match([russia, saudi], seed);
+
+console.log(match.occurrences.goals.value);
