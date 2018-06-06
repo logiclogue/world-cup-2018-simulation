105ecf5da6558ce56593adeb94065c2b8a818ea1
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-06 14:48:26 +0100

    Adds group

--- src/Group.js
+++ src/Group.js
@@ -3,14 +3,15 @@ const arrayHelpers = require("../src/arrayHelpers");
 const Match = require("football-score-sim").Match;
 
 class Group {
-    constructor(teams) {
+    constructor(teams, seed) {
         this.teams = teams;
+        this.seed = seed;
     }
 
     matches() {
         return roundrobin(this.teams.length, this.teams)
             .flatten()
-            .map(teams => new Match(teams));
+            .map((teams, i) => new Match(teams, this.seed.append(i)));
     }
 }
 
--- test/Group-test.js
+++ test/Group-test.js
@@ -4,6 +4,7 @@ const teamsJson = require("../src/teams.json");
 const Teams = require("../src/Teams");
 const Match = require("football-score-sim").Match;
 const Team = require("football-score-sim").Team;
+const Seed = require("football-score-sim").Seed;
 
 describe("Group", () => {
     const teams = new Teams(teamsJson);
@@ -11,7 +12,8 @@ describe("Group", () => {
     context("group A", () => {
         const teamList = teams.getByGroupName("A").value
             .map(obj => new Team(obj.name, obj.rating));
-        const group = new Group(teamList);
+        const seed = "testing".toSeed();
+        const group = new Group(teamList, seed);
 
         describe("#teams", () => {
             it("is a list of Team", () => {
@@ -26,10 +28,25 @@ describe("Group", () => {
                 expect(result).to.have.lengthOf(6);
             });
 
-            it("returns matches", () => {
-                expect(result[0]).to.be.instanceof(Match);
-                expect(result[0].home.name).to.equal("Russia");
-                expect(result[0].away.name).to.equal("Uruguay");
+            context("first match", () => {
+                const match = result[0];
+
+                it("is a match", () => {
+                    expect(match).to.be.instanceof(Match);
+                });
+
+                it("has a seed", () => {
+                    expect(match.seed).to.deep.equal(seed.append(0));
+                });
+
+                it("is between russia and uruguay", () => {
+                    expect(match.home.name).to.equal("Russia");
+                    expect(match.away.name).to.equal("Uruguay");
+                });
+
+                it("is 2-1", () => {
+                    expect(match.occurrences.goals.value).to.deep.equal([1, 2]);
+                });
             });
         });
     });
