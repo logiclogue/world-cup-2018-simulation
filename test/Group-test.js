ec78573807f859b5ea630302e80b6e557966226c
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-06 13:09:51 +0100

    Adding group, need teams now

---
+++ test/Group-test.js
@@ -0,0 +1,14 @@
+const Group = require("../src/Group");
+const teams = require("../src/teams.json");
+
+describe("Group", () => {
+    describe("#matches()", () => {
+        context("given 4 teams", () => {
+            const group = teams.filter()
+
+            it("has length of 6", () => {
+                $
+            });
+        });
+    });
+});
