66d7d6010e9172fc4a3b28a6fd5318eb553eb1c8
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-08 16:01:37 +0100

    Adds to expanded matches function

--- src/Matches.js
+++ src/Matches.js
@@ -1,6 +1,17 @@
+const ExpandedMatch = require("./ExpandedMatch");
+
 class Matches {
     constructor(matchArray) {
         this.value = matchArray;
+    }
+
+    toExpandedMatches() {
+        const homeTeams = this.value
+            .map(match => new ExpandedMatch(match.home, match));
+        const awayTeams = this.value
+            .map(match => new ExpandedMatch(match.away, match));
+
+        return homeTeams.concat(awayTeams);
     }
 }
 
--- test/Matches-test.js
+++ test/Matches-test.js
@@ -1,10 +1,29 @@
 const expect = require("chai").expect;
 const stubs = require("./stubs");
+const ExpandedMatch = require("../src/ExpandedMatch");
+const arrayHelpers = require("../src/arrayHelpers");
 
 describe("Matches", () => {
-    const matches = stubs.worldCup.getGroupByName("A").matches();
-
     describe("#toExpandedMatches()", () => {
-        
+        context("group A matches", () => {
+            const matches = stubs.worldCup.getGroupByName("A").matches();
+            const result = matches.toExpandedMatches();
+
+            it("has double the length of matches", () => {
+                expect(result).to.have.lengthOf(matches.value.length * 2);
+            });
+
+            it("is an array of ExpandedMatch", () => {
+                result.forEach(expandedMatch => {
+                    expect(expandedMatch).to.be.instanceOf(ExpandedMatch);
+                });
+            });
+
+            it("has no repeated columns", () => {
+                const expectedLength = result.unique().length;
+
+                expect(result).to.have.lengthOf(expectedLength);
+            });
+        });
     });
 });
