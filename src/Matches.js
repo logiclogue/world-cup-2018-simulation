8c86a0b0cd21e1bf677ce6fdcace6043a66fe4be
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-08 13:57:21 +0100

    Adding matches class

---
+++ src/Matches.js
@@ -0,0 +1,7 @@
+class Matches {
+    constructor(matchArray) {
+        this.value = matchArray;
+    }
+}
+
+module.exports = Matches;
---
+++ test/Matches-test.js
@@ -0,0 +1,7 @@
+const expect = require("chai").expect;
+
+describe("Matches", () => {
+    describe("#toExpandedMatches()", () => {
+        
+    });
+});
