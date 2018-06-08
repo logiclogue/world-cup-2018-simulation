503aa7270b5d3f7ee3de8feef40bd7bfade6a0dc
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-08 14:16:26 +0100

    Adds worldcup to stubs

--- test/Matches-test.js
+++ test/Matches-test.js
@@ -1,11 +1,8 @@
 const expect = require("chai").expect;
 const stubs = require("./stubs");
-const teams = require("../src/teams.json");
-const WorldCup = require("../src/WorldCup");
 
 describe("Matches", () => {
-    const worldCup = new WorldCup(teams, stubs.seed);
-    const matches = worldCup.getGroupByName("A").matches();
+    const matches = stubs.worldCup.getGroupByName("A").matches();
 
     describe("#toExpandedMatches()", () => {
         
--- test/stubs.js
+++ test/stubs.js
@@ -1,7 +1,11 @@
 const Seed = require("football-score-sim").Seed;
+const teams = require("../src/teams.json");
+const WorldCup = require("../src/WorldCup");
 
 const seed = "testing".toSeed();
+const worldCup = new WorldCup(teams, seed);
 
 module.exports = {
-    seed: seed
+    seed: seed,
+    worldCup: worldCup
 };
