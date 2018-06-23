4d49a9debbd8f4306f2c428e2660e7869c99296f
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-23 22:57:39 +0100

    Updates to use bootstrap

--- src/index.js
+++ src/index.js
@@ -23,19 +23,25 @@ function Match(props) {
     const match = props.match;
 
     return (
-        <div>
-            <span>{ match.home.name } </span>
-            <span>{ match.occurrences.goals.home } </span>
-            <span>{ match.occurrences.goals.away } </span>
-            <span>{ match.away.name } </span>
-        </div>
+        <table class="table">
+            <tbody style={{ width: "100%" }}>
+                <td style={{ width: "40%", textAlign: "right" }}>{ match.home.name } </td>
+                <td style={{ width: "20%", textAlign: "center" }}>{ match.occurrences.goals.home } - { match.occurrences.goals.away }</td>
+                <td style={{ width: "40%", textAlign: "left" }}>{ match.away.name } </td>
+            </tbody>
+        </table>
     );
 }
 
 function Matches(props) {
     const matches = props.matches;
 
-    return matches.map(match => <Match match={ match } />);
+    return (
+        <div>
+            <h3>{ props.name }</h3>
+            <div>{ matches.map(match => <Match match={ match } />) }</div>
+        </div>
+    );
 }
 
 ReactDOM.render(
