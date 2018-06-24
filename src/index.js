7f29b293b9b586fdd1e563f94492b85586b04081
logiclogue <jordanpaullord@hotmail.co.uk>
2018-06-24 22:57:03 +0100

    Adds groups

--- src/index.js
+++ src/index.js
@@ -9,14 +9,68 @@ function WorldCup(props) {
     const worldCup = new Sim(teams, seed.toSeed());
 
     return (
-        <div>
-            <Matches matches={ worldCup.getRoundOf16() } /><br />
-            <Matches matches={ worldCup.getQuarterFinals() } /><br />
-            <Matches matches={ worldCup.getSemiFinals() } /><br />
+        <div class="container">
+            <Group group={ worldCup.getGroupByName("A") } /><br />
+            <Matches name="Round of 16" matches={ worldCup.getRoundOf16() } /><br />
+            <Matches name="Quarter Finals" matches={ worldCup.getQuarterFinals() } /><br />
+            <Matches name="Semi Finals" matches={ worldCup.getSemiFinals() } /><br />
+            <h3>Final</h3>
             <Match match={ worldCup.getFinal() } />
             <p>{ worldCup.getWinner().name }</p>
         </div>
     );
+}
+
+function Group(props) {
+    const group = props.group;
+
+    return (
+        <div class="table">
+            <Table table={ group.table() } />
+            <Matches matches={ group.matches().value } />
+        </div>
+    );
+}
+
+function Table(props) {
+    const table = props.table;
+
+    return (
+        <table>
+            <thead>
+                <th>#</th>
+                <th>Team</th>
+                <th>P</th>
+                <th>W</th>
+                <th>D</th>
+                <th>L</th>
+                <th>GD</th>
+                <th>Pts</th>
+            </thead>
+            <tbody>
+                { table.map((record, i) =>
+                    <Record record={ record } position={ i + 1 } />) }
+            </tbody>
+        </table>
+    );
+}
+
+function Record(props) {
+    const record = props.record;
+    const position = props.position;
+
+    return (
+        <tr>
+            <td>{ position }</td>
+            <td>{ record.team.name }</td>
+            <td>{ record.played }</td>
+            <td>{ record.wins }</td>
+            <td>{ record.draws }</td>
+            <td>{ record.losses }</td>
+            <td>{ record.goalDifference }</td>
+            <td>{ record.points }</td>
+        </tr>
+    )
 }
 
 function Match(props) {
