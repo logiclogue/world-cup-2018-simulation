d7c5b89def6836f0d75677be71e084dbb871f527
logiclogue <jordanpaullord@hotmail.co.uk>
2018-07-15 10:56:05 +0100

    Updates to include bootstrap through webpack rather than the cdn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import RootDisplay from "./RootDisplay";
 import teamsJson from "./teams.json";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <RootDisplay seed="testing" teams={teamsJson} />,
