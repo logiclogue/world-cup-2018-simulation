251a3f3db519ff6c7de10ce4f045ec3e23256d70
logiclogue <jordanpaullord@hotmail.co.uk>
2018-07-15 11:01:50 +0100

    Updates seed to always randomise from the start

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import RootDisplay from "./RootDisplay";
 import teamsJson from "./teams.json";
+import md5 from "md5";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const seed = md5(Date.now() + "");
+
 ReactDOM.render(
-    <RootDisplay seed="testing" teams={teamsJson} />,
+    <RootDisplay seed={seed} teams={teamsJson} />,
     document.getElementById("root")
 );
