26496a7424b17cfeafc8b4b767a6090079a21f0b
logiclogue <jordanpaullord@hotmail.co.uk>
2018-07-24 09:00:38 +0100

    Removes console.log

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import teamsJson from "./teams.json";
 import md5 from "md5";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-console.log(addFive(43));
-
 const seed = md5(Date.now() + "");
 
 ReactDOM.render(
