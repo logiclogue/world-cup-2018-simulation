af11afd09bc76ca636ed51f7be3ac2283f2cdf10
logiclogue <jordanpaullord@hotmail.co.uk>
2018-07-24 08:59:37 +0100

    Sets everything up

---
+++ src/AddFive.purs
@@ -0,0 +1,6 @@
+module AddFive(addFive) where
+
+import Prelude ((+))
+
+addFive :: Number -> Number
+addFive x = x + 5.0
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import RootDisplay from "./RootDisplay";
 import teamsJson from "./teams.json";
 import md5 from "md5";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { addFive } from "./AddFive.purs";
+
+console.log(addFive(43));
 
 const seed = md5(Date.now() + "");
 
